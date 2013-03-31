7061e87a976f1e7d3061e30c514bff0610fc2ac5
Tapal Mahammad Saleem <saleemquest@yahoo.com>
2013-03-31 16:13:46 +0530

    ghfh mnjkj

---
+++ src/main/java/com/ignitedcoders/app/Test.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.ignitedcoders.app;
+
+
+
+/**
+ * @author c_rsharv
+ *
+ */
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Tesitng");
+		
+		System.out.println("Hello World");
+
+	}
+	
+}
---
+++ src/test/java/UrlValidatorTest.java
@@ -0,0 +1,16 @@
+
+import junit.framework.TestCase;
+
+
+
+public class UrlValidatorTest extends TestCase {
+	public void testNotValid() {
+		assertFalse(false);
+	}
+
+//	//temporary for testing the bamboo hungbuildkiller plugin
+
+//	public void testBuildTimeout() throws Exception {
+//		Thread.sleep(1000*60*10);
+//	}
+}
